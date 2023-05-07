e39990a3b4c22cb89259a92656c8aac07984de14
JoelCoster <joelcoster@hotmail.com>
2023-05-07 21:20:06 +0200

    Added basic turn management, dummy dialog
    Added basic turn taking functionality and a dummy dialog system for testing purposes

--- speech.py
+++ speech.py
@@ -3,23 +3,25 @@ from gtts import gTTS
 from io import BytesIO
 from pygame import mixer
 import time
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
 
-language = 'en'
+colorama_init()
 
 
 def listen():
-    r = sr.Recognizer()
+    stt = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 1
-        audio = r.listen(source)
+        print(f"{Fore.GREEN}Listening...{Style.RESET_ALL}")
+        stt.pause_threshold = 1
+        audio = stt.listen(source)
 
     try:
-        print("Recognizing...")
-        query = r.recognize_google(audio, language='en-us')
-        print(query)
-        speak(query)
+        print(f"{Fore.GREEN}Recognizing...{Style.RESET_ALL}")
+        query = stt.recognize_google(audio, language='en-us')
+        print(f"{Fore.CYAN}User: {Style.RESET_ALL}{query}")
 
     except Exception as e:
         print(e)
@@ -30,6 +32,9 @@ def listen():
 
 def speak(text):
     mp3_fp = BytesIO()
+
+    print(f"{Fore.MAGENTA}System: {Style.RESET_ALL}{text}")
+
     tts = gTTS(text, lang='en')
     tts.write_to_fp(mp3_fp)
 
@@ -41,6 +46,37 @@ def speak(text):
     while mixer.music.get_busy():
         time.sleep(0.1)
 
+
+def dialogManagement(text):
+    text = text.lower()
+    triggers = ["talk to you later", "better luck next time", "see you"]
+    for trigger in triggers:
+        if trigger in text:
+            return False, "Till next time"
+        
+    triggers = ["how are you", "how are you doing", "good how about you"]
+    for trigger in triggers:
+        if trigger in text:
+            return True, "Mediocre at best"
+        
+    triggers = ["hello", "hey"]
+    for trigger in triggers:
+        if trigger in text:
+            return True, "Hey how are you?"
+
+    return True, "Sorry but I don't know how to respond to that"
+
    
 
-listen()
+def turnManagement(start_conversation=True): #need conversation log as well
+    if start_conversation:
+        speak("Hello, I am Snoop")
+
+    continue_conversation = True
+    while continue_conversation:
+        text = listen()
+        continue_conversation, response = dialogManagement(text)
+        speak(response)
+
+
+turnManagement()

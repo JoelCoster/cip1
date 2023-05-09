95c594316b93e88253a96190d4ba7397cebe01b1
thijmendam <thijmenthijmen@live.nl>
2023-05-09 10:01:25 +0200

    Added README with development instructions, improved repository structure, added requirements.txt

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Snoop
+
+Best chatbot ever.
+
+## Development Instructions
+
+1. If you have a Mac, you need the PortAudio API, which is a requirement for the Python package `pyaudio`. You can 
+   install it using `brew install portaudio`.
+2. Install all required packages: `pip install -r requirements.txt`
--- src/snoop.py
+++ src/snoop.py
@@ -1,28 +1,28 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import speech as sr
+import time
+import sys
+import re
 
-import speech_recognition as sr
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 from gtts import gTTS
 from io import BytesIO
-import time
 from colorama import init as colorama_init
 from colorama import Fore
 from colorama import Style
 from datetime import datetime
 from pygame import mixer
-import sys
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
-import re
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 colorama_init()
-global mic_index
 mic_index = 0
 
 
 def listen():
     stt = sr.Recognizer()
 
-    with sr.Microphone(device_index = mic_index) as source:
+    with sr.Microphone(device_index=mic_index) as source:
         print(f"{Fore.GREEN}Listening...{Style.RESET_ALL}")
         stt.pause_threshold = 1
         audio = stt.listen(source)
@@ -65,10 +65,10 @@ class ChatLog:
 
     def getLog(self):
         return self.log
-    
+
     def getParticipantLog(self, participant):
         return [line for line in self.log if line[0] == participant]
-    
+
     def exportCSV(self):
         exist = os.path.exists("logs")
         if not exist:
@@ -85,11 +85,10 @@ class Snoop:
     def __init__(self):
         self.chatlog = ChatLog()
         model_name = "facebook/blenderbot-400m-distill"
-        #model_name = "facebook/blenderbot-1B-distill"
-        #model_name = "facebook/blenderbot-3B"
+        # model_name = "facebook/blenderbot-1B-distill"
+        # model_name = "facebook/blenderbot-3B"
         self.model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
         self.tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
-
 
     def dialogManagement(self, text):
         text = text.lower()
@@ -102,24 +101,29 @@ class Snoop:
         reply_ids = self.model.generate(**inputs, max_new_tokens=100)
 
         return True, re.sub(r'<.*?>', r'', self.tokenizer.batch_decode(reply_ids)[0]).strip()
-   
 
     def run(self, start_conversation=True, speech=False):
         if start_conversation:
             initial_utterance = "Hello, I am Snoop"
-            if speech: speak(initial_utterance)
-            else: print(f"{Fore.MAGENTA}System: {Style.RESET_ALL}{initial_utterance}")
+            if speech:
+                speak(initial_utterance)
+            else:
+                print(f"{Fore.MAGENTA}System: {Style.RESET_ALL}{initial_utterance}")
             self.chatlog.addLine("Snoop", initial_utterance)
 
         continue_conversation = True
         while continue_conversation:
-            if speech: text = listen()
-            else: text = input(f"{Fore.CYAN}User: {Style.RESET_ALL}")
+            if speech:
+                text = listen()
+            else:
+                text = input(f"{Fore.CYAN}User: {Style.RESET_ALL}")
             if text:
                 self.chatlog.addLine("User", text)
                 continue_conversation, response = self.dialogManagement(text)
-                if speech: speak(response)
-                else: print(f"{Fore.MAGENTA}System: {Style.RESET_ALL}{response}")
+                if speech:
+                    speak(response)
+                else:
+                    print(f"{Fore.MAGENTA}System: {Style.RESET_ALL}{response}")
                 self.chatlog.addLine("Snoop", response)
 
         self.chatlog.exportCSV()
@@ -135,5 +139,5 @@ if __name__ == "__main__":
         speech = True
     if "--start" in sys.argv:
         start_converstation = True
-        
+
     snoop.run(start_conversation=start_converstation, speech=speech)
